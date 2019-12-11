3ad5994841f2e86be8d43d9b8a237a3779ef9c9a
EliHasselbring <elihasselbring@hotmail.com>
2019-12-10 23:43:52 -0600

    Sorry

--- public/js/drops.js
+++ public/js/drops.js
@@ -1,9 +1,30 @@
-drop = {
+function getRand(min, max) { //needed for the randomDrop function
+    var diff = max - (min);
+    return Math.floor((Math.random()) * diff - (max));
+ }
+ 
+ function randomLoc(dropLocs) {
+    spawnLoc = this.getRand(0, (dropLocs.length));
+    return spawnLoc;
+ }
+ 
+ drop = {
     "type": null,
-    "location":null,
-    "color":null
-}
-
-function randomDrop() {
-
-}
+    "loc": [50, 50] //randomLoc(/*pass in map.locs*/),
+ }
+ 
+ var exp = {};
+ 
+ exp.randomDrop = function() { //returns random drop to the server just 2 for now
+    randNum = getRand(1, 10);
+ 
+    if (randNum >= 1 && randNum <= 9) {
+       this.drop.type = "shield";
+    }
+    if (randNum == 10) {
+       this.drop.type = "shield";
+    }
+    return drop;
+ }
+ 
+ exports.data = exp;
